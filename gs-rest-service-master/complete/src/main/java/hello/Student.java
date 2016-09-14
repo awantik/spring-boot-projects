a206716e37da57942ad0df69610d608292bd62ec
curanest <curanest@gmail.com>
2016-09-14 12:45:45 +0530

    first commit

---
+++ gs-rest-service-master/complete/src/main/java/hello/Student.java
@@ -0,0 +1,21 @@
+package hello;
+
+public class Student {
+	
+	private final long id;
+    private final String content;
+
+    public Student(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
